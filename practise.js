cfeb0b45ef6582387842afc2d3bd487700112f0c
ShekharRedd <shekharreddy4558@gmail.com>
2023-07-28 15:53:54 +0530

    first js

--- practise.js
+++ practise.js
@@ -40,3 +40,45 @@ const main1=async ()=>{
 }
 
 console.log("hi")
+
+
+  
+async function ram(){
+  let addd= new Promise((resolve , reject) => {
+     setTimeout(()=>{
+      resolve("hello")
+     },3000)
+  })
+  let bddd= new Promise((resolve , reject) => {
+    setTimeout(()=>{
+     resolve("hello2")
+    },5000)
+ })
+
+
+ console.log("starting from a")
+let r1=await addd
+console.log("starting from a",r1)
+
+console.log("starting from b")
+let r2=await bddd
+console.log("starting from b",r2)
+return [r1,r2]
+}
+
+
+
+let chery=async ()=>{
+  console.log("hello world")
+}
+const main1=async ()=>{
+
+  let a= await ram()
+  let b=await chery()
+  // a.then((va)=>{
+  //   console.log("hhesf",va)
+  // })
+  // a.catch((va)=>{
+  //   console.log(va)
+  // })
+console.lo
