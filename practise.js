42239c85d619b54f6552b5b0dafb7f461bc173b0
ShekharRedd <shekharreddy4558@gmail.com>
2023-07-28 15:52:48 +0530

    Sdf

--- practise.js
+++ practise.js
@@ -4,3 +4,14 @@
      setTimeout(()=>{
       resolve("hello")
      },3000)
+
+
+     console.log("sdfhj")
+     console.log("sdfhj")
+     valuesArray     console.log("sdfhj")
+     valuesArrayv
+     valuesArrayb
+     bn
+     navigator
+     
+
